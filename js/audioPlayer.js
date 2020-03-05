a7d428bcd9ad3a6d602360b71ef2e90bf62b7df5
Alexey Savchenko <wasxela@gmail.com>
2020-03-05 19:05:37 +0200

    layout start modal window

--- js/audioPlayer.js
+++ js/audioPlayer.js
@@ -4,27 +4,36 @@ export class audioPlayer {
 		this.renderAudioPlayer();
 		this.btnAudio = document.querySelector('.btnAudio');
 		this.audioVolume = document.querySelector('.audioVolume');
-		this.audio = new Audio('./media/Neil Young - Dead Man Theme.wav');
+		this.audioTtrack = new Audio('./media/Neil Young.mp3');
+
+
 		this.btnAudio.addEventListener('click', this.clickAudio.bind(this));
-		this.loadAudio();
+
 	}
 	renderAudioPlayer(){
 		this.audioPlayer.innerHTML = `<p>audio control</p>
 									  <div class="audioControl d-flex">
 										  <div class="audioVolume"></div>
-										  <div class="btnAudio btnAudio-pause"></div>
+										  <div class="btnAudio btnAudio-play"></div>
 									  </div>`
 	}
-	loadAudio(){
-		document.addEventListener('DOMContentLoaded', ()=> {
-			this.audio.autoplay = true;
-			this.audio.loop = true;
-			 this.audioPlayer.appendChild(this.audio);
-
-		});
-	}
+	// loadAudio(){
+	// 	document.addEventListener('DOMContentLoaded', ()=> {
+	// 		console.log('DOM loaded');
+	// 		this.audio.autoplay = true;
+	// 		this.audio.loop = true;
+	// 		this.audioPlayer.appendChild(this.audio);
+	//
+	// 	});
+	// }
 	clickAudio(){
-
+		if(this.btnAudio.classList.contains('btnAudio-pause')){
+			this.audioTtrack.loop = true;
+			this.audioTtrack.play();
+		}
+		else this.audioTtrack.pause();
+		this.btnAudio.classList.toggle('btnAudio-pause');
+		this.btnAudio.classList.toggle('btnAudio-play');
 	}
 
 }
